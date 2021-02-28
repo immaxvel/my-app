05bcf079935143b63898239bf872b5185abff01b
Maksim Velichko <immaxvel@gmail.com>
2021-02-28 21:24:39 +0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Rockstar_Games_Logo.svg/320px-Rockstar_Games_Logo.svg.png"></img>
+    </header>
+  );
+};
+
+export default Header;
